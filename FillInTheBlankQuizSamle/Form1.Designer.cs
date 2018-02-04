31f9720598d2933cef3433300ff87769b797bb81
Reza Aghaei <rag@sitecore.net>
2018-02-04 14:41:49 +0800

    Sample created.

--- FillInTheBlankQuizSamle/Form1.Designer.cs
+++ FillInTheBlankQuizSamle/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 32);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(516, 272);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(516, 32);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(136, 29);
+            this.toolStripButton1.Text = "Show Result";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(82, 29);
+            this.toolStripButton2.Text = "Reset";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 304);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
     }
 }
 
--- FillInTheBlankQuizSamle/Form1.cs
+++ FillInTheBlankQuizSamle/Form1.cs
@@ -16,5 +16,45 @@ namespace FillInTheBlankQuizSamle
         {
             InitializeComponent();
         }
+        Quiz quiz;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            quiz = new Quiz();
+            quiz.Text = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+            quiz.Ranges.Add(new SelectionRange(6, 5));
+            quiz.Ranges.Add(new SelectionRange(30, 7));
+            quiz.Ranges.Add(new SelectionRange(61, 2));
+            quiz.Ranges.Add(new SelectionRange(82, 6));
+
+            this.webBrowser1.DocumentText = quiz.Render();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < quiz.Ranges.Count; i++)
+            {
+                var id = $"q{i}";
+                var input = webBrowser1.Document.GetElementById(id);
+                var value = input.GetAttribute("value");
+                if (quiz.Text.Substring(quiz.Ranges[i].Start, quiz.Ranges[i].Length) == value)
+                    this.webBrowser1.Document.InvokeScript("setCorrect", new[] { id });
+                else
+                    this.webBrowser1.Document.InvokeScript("setWrong", new[] { id });
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < quiz.Ranges.Count; i++)
+            {
+                var id = $"q{i}";
+                var input = webBrowser1.Document.GetElementById(id);
+                var value = input.GetAttribute("value");
+                if (quiz.Text.Substring(quiz.Ranges[i].Start, quiz.Ranges[i].Length) == value)
+                    this.webBrowser1.Document.InvokeScript("setCorrect", new[] { id });
+                else
+                    this.webBrowser1.Document.InvokeScript("setWrong", new[] { id });
+            }
+        }
     }
 }
